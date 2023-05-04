c166ecc37fef77d8ea2d882c35bbc1c86b7a6508
VitaliyDunyushkin <117518206+VitaliyDunyushkin@users.noreply.github.com>
2023-05-04 22:14:34 +0300

    add trim

--- js/task-05.js
+++ js/task-05.js
@@ -5,7 +5,7 @@ inputName.addEventListener("input", handleInput);
 
 function handleInput() {
   if (inputName.value !== "") {
-    spanOutput.textContent = inputName.value;
+    spanOutput.textContent = inputName.value.trim();
   } else {
     spanOutput.textContent = "Anonymous";
   }
